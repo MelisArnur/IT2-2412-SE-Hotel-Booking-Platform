a299066696037fcddaa588373e8e12b6d682217d
Melis Arnur <melisarnur3@gmail.com>
2025-05-03 20:40:03 +0500

    Implemented Hotel model, migrations, shell data, and admin setup (Lab 2)

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Hotel
 
-# Register your models here.
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']  # Fields to display in the list
+    search_fields = ['name']  # Search by name
+    list_filter = ['created_at']  # Filter by creation date
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=255)  # Hotel name
+    description = models.TextField(blank=True, null=True)  # Description
+    created_at = models.DateTimeField(auto_now_add=True)  # Creation date
+
+    def __str__(self):
+        return self.name
