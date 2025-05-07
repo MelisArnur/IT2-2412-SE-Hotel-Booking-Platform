e43708c6558a926f5535e86e4adf3f7c45a4ada5
Melis Arnur <melisarnur3@gmail.com>
2025-05-07 23:14:30 +0500

    Implemented CRUD operations for hotels in Lab 4 (Hotel Booking Platform)

--- booking/models.py
+++ booking/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Hotel(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+    
