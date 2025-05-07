327931c8af54940cf5014bf9d067e99fa72ed5e4
Melis Arnur <melisarnur3@gmail.com>
2025-05-08 01:20:13 +0500

    Implemented  operations authorization and authentication of users for hotels in Lab 5 (Hotel Booking Platform)

--- booking/urls.py
+++ booking/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
@@ -8,4 +9,9 @@ urlpatterns = [
     path('hotels/create/', views.HotelCreateView.as_view(), name='hotel-create'),
     path('hotels/<int:pk>/update/', views.HotelUpdateView.as_view(), name='hotel-update'),
     path('hotels/<int:pk>/delete/', views.HotelDeleteView.as_view(), name='hotel-delete'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit-profile'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth import login
 from .models import Hotel
-from .forms import HotelForm
+from .forms import HotelForm, RegisterForm, UpdateProfileForm
 
 def home(request):
     hotels = Hotel.objects.all()
@@ -11,24 +15,62 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-class HotelListView(ListView):
+class HotelListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = Hotel
     template_name = 'hotel_list.html'
     context_object_name = 'hotels'
 
-class HotelCreateView(CreateView):
+    def test_func(self):
+        return self.request.user.username == 'Arnur'  # Разрешаем только admin
+
+class HotelCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Hotel
     form_class = HotelForm
     template_name = 'hotel_form.html'
     success_url = reverse_lazy('hotel-list')
 
-class HotelUpdateView(UpdateView):
+    def test_func(self):
+        return self.request.user.username == 'Arnur'  # Разрешаем только admin
+
+class HotelUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Hotel
     form_class = HotelForm
     template_name = 'hotel_form.html'
     success_url = reverse_lazy('hotel-list')
 
-class HotelDeleteView(DeleteView):
+    def test_func(self):
+        return self.request.user.username == 'Arnur'  # Разрешаем только admin
+
+class HotelDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Hotel
     template_name = 'hotel_confirm_delete.html'
     success_url = reverse_lazy('hotel-list')
+
+    def test_func(self):
+        return self.request.user.username == 'Arnur'  # Разрешаем только admin
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UpdateProfileForm(instance=request.user)
+    return render(request, 'edit_profile.html', {'form': form})
